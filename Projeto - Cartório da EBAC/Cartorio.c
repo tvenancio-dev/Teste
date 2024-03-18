cd6599d098459b9fb53311c50edc50d1c95d6d73
tvenancio-dev <163868254+tvenancio-dev@users.noreply.github.com>
2024-03-18 11:37:32 -0300

    Criando repositório de teste

---
+++ Projeto - Cartório da EBAC/Cartorio.c
@@ -0,0 +1,14 @@
+#include <stdio.h> //biblioteca de comunicação com o usuário
+#include <stdlib.h> //biblioteca de alocação de espaço em memória
+#include <locale.h> //biblioteca de alocações de texto por região
+
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	printf("### Cartório da EBAC ###\n\n");
+	printf("Escolha a opção desejada do menu:\n\n");
+	printf("\t1 - Registrar nomes;\n");
+	printf("\t2 - Consultar nomes;\n");
+	printf("\t3 - Deletar nomes.\n\n");
+	printf("Esse software é de livre uso dos alunos da EBAC.\n");
+}
